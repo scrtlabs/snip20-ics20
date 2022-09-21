f679e0312ec7f6088056390505592093c21686ee
Assaf Morami <assaf.morami@gmail.com>
2022-09-21 14:36:51 +0300

    tests

--- test/test.ts
+++ test/test.ts
@@ -190,7 +190,7 @@ beforeAll(async () => {
         label: `ics20-${Date.now()}`,
       }),
     ],
-    { gasLimit: 300_000 }
+    { gasLimit: 5_000_000 }
   );
   if (tx.code !== TxResultCode.Success) {
     console.error(tx.rawLog);
@@ -211,9 +211,6 @@ beforeAll(async () => {
   await waitForIBCConnection("secretdev-1", "http://localhost:9091");
   await waitForIBCConnection("secretdev-2", "http://localhost:9391");
 
-  await waitForIBCChannel("secretdev-1", "http://localhost:9091", "channel-0");
-  await waitForIBCChannel("secretdev-2", "http://localhost:9391", "channel-0");
-
   console.log("Creating IBC channel...");
 
   const command =
@@ -223,18 +220,17 @@ beforeAll(async () => {
     "--a-chain secretdev-1 --a-connection connection-0 " +
     `--b-port transfer --a-port ${contracts.ics20.ibcPortId}`;
 
-  console.log(command);
+  // console.log(command);
 
   const result = execSync(command);
 
   const trimmedResult = result.toString().replace(/\s|\n/g, "");
 
-  const regexChannel1 = /,channel_id:Some\(ChannelId\("(channel-\d+)"/;
+  const regexChannel1 = /a_side.+?,channel_id:Some\(ChannelId\("(channel-\d+)"/;
   channelId1 = regexChannel1.exec(trimmedResult)[1];
   expect(channelId1).toContain("channel-");
 
-  const regexChannel2 =
-    /,counterparty_channel_id:Some\(ChannelId\("(channel-\d+)"/;
+  const regexChannel2 = /b_side.+?,channel_id:Some\(ChannelId\("(channel-\d+)"/;
   channelId2 = regexChannel2.exec(trimmedResult)[1];
   expect(channelId2).toContain("channel-");
 
@@ -243,57 +239,73 @@ beforeAll(async () => {
 }, 180_000 /* 3 minutes timeout */);
 
 test("send from 1 to 2", async () => {
-  let tx = await accounts1[0].secretjs.tx.broadcast([
-    new MsgExecuteContract({
-      sender: accounts1[0].address,
-      contractAddress: contracts.ics20.address,
-      codeHash: contracts.ics20.codeHash,
-      msg: {
-        register_tokens: {
-          tokens: [
-            {
-              address: contracts.snip20.address,
-              code_hash: contracts.snip20.address,
-            },
-          ],
+  let tx = await accounts1[0].secretjs.tx.broadcast(
+    [
+      new MsgExecuteContract({
+        sender: accounts1[0].address,
+        contractAddress: contracts.ics20.address,
+        codeHash: contracts.ics20.codeHash,
+        msg: {
+          register_tokens: {
+            tokens: [
+              {
+                address: contracts.snip20.address,
+                code_hash: contracts.snip20.codeHash,
+              },
+            ],
+          },
         },
-      },
-    }),
-    new MsgExecuteContract({
-      sender: accounts1[0].address,
-      contractAddress: contracts.snip20.address,
-      codeHash: contracts.snip20.codeHash,
-      msg: {
-        send: {
-          recipient: contracts.ics20.address,
-          recipient_code_hash: contracts.ics20.codeHash,
-          amount: "1",
-          msg: toBase64(
-            toUtf8(
-              JSON.stringify({
-                channel: channelId1,
-                remote_address: accounts2[1].address,
-                timeout: 10 * 60, // 10 minutes
-              })
-            )
-          ),
+      }),
+      new MsgExecuteContract({
+        sender: accounts1[0].address,
+        contractAddress: contracts.snip20.address,
+        codeHash: contracts.snip20.codeHash,
+        msg: {
+          send: {
+            recipient: contracts.ics20.address,
+            recipient_code_hash: contracts.ics20.codeHash,
+            amount: "1",
+            msg: toBase64(
+              toUtf8(
+                JSON.stringify({
+                  channel: channelId1,
+                  remote_address: accounts2[1].address,
+                  timeout: 10 * 60, // 10 minutes
+                })
+              )
+            ),
+          },
         },
-      },
-    }),
-  ]);
+      }),
+    ],
+    {
+      gasLimit: 5_000_000,
+    }
+  );
   if (tx.code !== TxResultCode.Success) {
     console.error(tx.rawLog);
   }
   expect(tx.code).toBe(TxResultCode.Success);
 
+  console.log(tx.arrayLog);
+
   console.log("Waiting for balance on secretdev-2");
 
+  const expectedIbcDenom = ibcDenom(
+    [{ portId: contracts.ics20.ibcPortId, channelId: channelId1 }],
+    `cw20:${contracts.snip20.address}`
+  );
+
   while (true) {
-    const balances = await accounts2[1].secretjs.query.bank.allBalances({
+    execSync(
+      `docker exec test-relayer-1 hermes clear packets --chain secretdev-2 --port transfer --channel ${channelId2}`
+    );
+    const { balances } = await accounts2[1].secretjs.query.bank.allBalances({
       address: accounts2[1].address,
     });
-    console.log(balances);
-    sleep(1000);
+
+    console.log(`expectedDenom: ${expectedIbcDenom}`, balances);
+    await sleep(5000);
   }
 });
 
